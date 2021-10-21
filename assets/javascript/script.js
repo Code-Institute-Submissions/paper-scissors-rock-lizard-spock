f56cb041c7c7a268da354381fc884d941674841d
r3b3l-dev <wayne.manning@tuta.io>
2021-10-21 08:07:28 +0000

    Add the check winner function into main game function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -40,13 +40,114 @@ const loserResetButton = document.getElementById("loserReset");
   let computerChoice = Math.floor(Math.random() * 5);
   computerImage.src = `assets/images/computer-${choices[computerChoice]}.png`;
   let result = checkWinner(choices[playerChoice], choices[computerChoice]);
+
+  checkWinner(result);
 }
 
 /**
  * Compares playerChoice and computerChoice data strings to check winner of game
  */
-function checkWinner() {
-
+ function checkWinner(playerChoice, computerChoice) {
+  /* If player chooses rock */
+  if (playerChoice === "rock" && computerChoice === "rock") {
+    document.getElementById("messages").innerHTML = "Draw!";
+  }
+  else if (playerChoice === "rock" && computerChoice === "paper") {
+    document.getElementById("messages").innerHTML = "You Lose! Paper Covers Rock";
+    addComputerScore();
+  }
+  else if (playerChoice === "rock" && computerChoice === "scissors") {
+    document.getElementById("messages").innerHTML = "You Win! Rock Crushes Scissors";
+    addUserScore();
+  }
+  else if (playerChoice === "rock" && computerChoice === "lizard") {
+    document.getElementById("messages").innerHTML = "You Win! Rock Crushes Lizard";
+    addUserScore();
+  }
+  else if (playerChoice === "rock" && computerChoice === "spock") {
+    document.getElementById("messages").innerHTML = "You Lose! Spock Vaporizes Rock";
+    addComputerScore();
+  }
+  /* If chooses paper */
+  else if (playerChoice === "paper" && computerChoice === "paper") {
+    document.getElementById("messages").innerHTML = "Draw!";
+  }
+  else if (playerChoice === "paper" && computerChoice === "rock") {
+    document.getElementById("messages").innerHTML = "You Win! Paper Covers Rock";
+    addUserScore();
+  }
+  else if (playerChoice === "paper" && computerChoice === "scissors") {
+    document.getElementById("messages").innerHTML = "You Lose! Scissors Cuts Paper";
+    addComputerScore();
+  }
+  else if (playerChoice === "paper" && computerChoice === "lizard") {
+    document.getElementById("messages").innerHTML = "You Lose! Lizard Eats Paper";
+    addComputerScore();
+  }
+  else if (playerChoice === "paper" && computerChoice === "spock") {
+    document.getElementById("messages").innerHTML = "You Win! Paper Disproves Spock";
+    addUserScore();
+  }
+  /* If player chooses scissors */
+  else if (playerChoice === "scissors" && computerChoice === "scissors") {
+    document.getElementById("messages").innerHTML = "Draw!";
+  }
+  else if (playerChoice === "scissors" && computerChoice === "rock") {
+    document.getElementById("messages").innerHTML = "You Lose! Rock Crushes Scissors";
+    addComputerScore();
+  }
+  else if (playerChoice === "scissors" && computerChoice === "paper") {
+    document.getElementById("messages").innerHTML = "You Win! Scissors Cuts Paper";
+    addUserScore();
+  }
+  else if (playerChoice === "scissors" && computerChoice === "lizard") {
+    document.getElementById("messages").innerHTML = "You Win! Scissors Decapitates Lizard";
+    addUserScore();
+  }
+  else if (playerChoice === "scissors" && computerChoice === "spock") {
+    document.getElementById("messages").innerHTML = "You Lose! Spock Smashes Scissors";
+    addComputerScore();
+  }
+  /* If player chooses lizard */
+  else if (playerChoice === "lizard" && computerChoice === "lizard") {
+    document.getElementById("messages").innerHTML = "Draw!";
+  }
+  else if (playerChoice === "lizard" && computerChoice === "rock") {
+    document.getElementById("messages").innerHTML = "You Lose! Rock Crushes Lizard";
+    addComputerScore();
+  }
+  else if (playerChoice === "lizard" && computerChoice === "paper") {
+    document.getElementById("messages").innerHTML = "You Win! Lizard Eats Paper";
+    addUserScore();
+  }
+  else if (playerChoice === "lizard" && computerChoice === "scissors") {
+    document.getElementById("messages").innerHTML = "You Lose! Scissors Decapitates Lizard";
+    addComputerScore();
+  }
+  else if (playerChoice === "lizard" && computerChoice === "spock") {
+    document.getElementById("messages").innerHTML = "You Win! Lizard Poisons Spock";
+    addUserScore();
+  }
+  /* If player chooses spock */
+  else if (playerChoice === "spock" && computerChoice === "spock") {
+    document.getElementById("messages").innerHTML = "Draw!";
+  }
+  else if (playerChoice === "spock" && computerChoice === "rock") {
+    document.getElementById("messages").innerHTML = "You Win! Spock Vaporizes Rock ";
+    addUserScore();
+  }
+  else if (playerChoice === "spock" && computerChoice === "paper") {
+    document.getElementById("messages").innerHTML = "You Lose! Paper Disproves Spock";
+    addComputerScore();
+  }
+  else if (playerChoice === "spock" && computerChoice === "scissors") {
+    document.getElementById("messages").innerHTML = "You Win! Spock Smashes Scissors";
+    addUserScore();
+  }
+  else if (playerChoice === "spock" && computerChoice === "lizard") {
+    document.getElementById("messages").innerHTML = "You Lose! Lizard Poisons Spock";
+    addUserScore();
+  }
 }
 
 /**
