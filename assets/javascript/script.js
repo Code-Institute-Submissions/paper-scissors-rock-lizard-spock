62b8077fd46206b5a63d3b4f4ab41dad6fa730a2
r3b3l-dev <wayne.manning@tuta.io>
2021-10-21 00:26:00 +0000

    Cache the DOM elements from HTML page

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+/* Cache DOM elements */
+
+const buttons = document.getElementsByClassName("control");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const resetButton = document.getElementById("reset");
+const winnerResetButton = document.getElementById("winnerReset");
+const loserResetButton = document.getElementById("loserReset");
+/*const winMessages = ["You are lucky today", "Well done rock star", "Superstar wins again"];*/
+/*const loserMessages = ["You are unlucky today", "Well done to the computer", "Today is not your day :("];*/
