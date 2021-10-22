7fe4dc8676c29e4b20c3d84b75dd2aa37ab36244
r3b3l-dev <wayne.manning@tuta.io>
2021-10-22 08:53:39 +0000

    Remove unused code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,8 +8,6 @@ const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 const resetButton = document.getElementById("reset");
 const winnerResetButton = document.getElementById("winnerReset");
 const loserResetButton = document.getElementById("loserReset");
-/*const winMessages = ["You are lucky today", "Well done rock star", "Superstar wins again"];*/
-/*const loserMessages = ["You are unlucky today", "Well done to the computer", "Today is not your day :("];*/
 
 /**
  * Function to run scrolling title text displaying name of game
